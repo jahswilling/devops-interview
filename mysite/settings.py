3e5881e394e76a45c398b6851868ce281b150e8c
jahswilling <jahswilling@gmail.com>
2023-12-28 18:36:34 +0100

    DevOps Assessment test solution

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5v$ll=8yw@o&5g+awna-3w*nc38f%d69ms!4ito+7^94narw5j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["34.27.124.173","127.0.0.1"]
 
 
 # Application definition
